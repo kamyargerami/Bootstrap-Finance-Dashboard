fc48f64198b1134102769633ea18b7821e694669
kamyar <gerami.kamyar@yahoo.com>
2021-01-27 22:59:42 +0330

    change nav styles

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
-function openNav() {
-    $('#sidenav').css('left', '0');
-}
-
-function closeNav() {
-    $('#sidenav').css('left', '-250px');
+function toggle_sidenav() {
+    if ($('#sidenav').css('left') == '-250px') {
+        $('#sidenav').css('left', '0');
+    } else {
+        $('#sidenav').css('left', '-250px');
+    }
 }
